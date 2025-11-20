7e6bb96ccb8383015815a41889b25a50db6f95d2
Volodymyr Poliakov <vladimir03002@gmail.com>
2025-11-19 19:44:01 -0600

    sending_mult_values2

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,8 @@ use std::time::Duration;
 fn main() {
     let (tx, rx) = mpsc::channel();
 
+    let tx1 = tx.clone();
+
     thread::spawn(move || {
         let vals = vec![
             String::from("hi"),
@@ -19,6 +21,20 @@ fn main() {
         }
     });
 
+    thread::spawn(move || {
+        let vals = vec![
+            String::from("more"),
+            String::from("messages"),
+            String::from("for"),
+            String::from("you"),
+        ];
+
+        for val in vals {
+            tx1.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
+    
     for received in rx {
         println!("Got: {}", received);
     }
