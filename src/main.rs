f802c134bc49b6d784cfde65298e6a7380766021
Volodymyr Poliakov <vladimir03002@gmail.com>
2025-11-19 20:37:57 -0600

    solving_with_arc<t>

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,24 @@
-use std::sync::Mutex;
+use std::sync::{Arc, Mutex};
+use std::thread;
 
 fn main() {
-    let m = Mutex::new(0);
+    let counter = Arc::new(Mutex::new(0));
+    let mut handles = vec![];
 
-    {
-        println!("m = {:?}", m);
-        let mut num = m.lock().unwrap();
-        *num = 6;
-        println!("m = {:?}", m);
+    for _ in 0..10 {
+        let counter = Arc::clone(&counter);
+        let handle = thread::spawn({ move || {
+                let mut num = counter.lock().unwrap();
+                
+                *num += 1;
+            }
+        });
+        handles.push(handle);
     }
-    println!("m = {:?}", m);
+
+    for handle in handles {
+        handle.join().unwrap();
+    }
+
+    println!("Result: {}", *counter.lock().unwrap());
 }
